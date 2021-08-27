d4c34600a4ee83732f0cd6ad4980c0e648bb76b7
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-08-27 17:43:31 -0300

    adicionando swiper funcionalidade para depoimentos

--- main.js
+++ main.js
@@ -26,3 +26,18 @@ window.addEventListener('scroll', () => {
 
   header.classList.remove('scroll');
 });
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true,
+    },
+  },
+});
