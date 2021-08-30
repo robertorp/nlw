e05d65f6b7f4d11dacf4c75bb2d01e52fab4f0dc
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-08-30 11:22:53 -0300

    configurado o ScrollReveal

--- main.js
+++ main.js
@@ -41,3 +41,21 @@ const swiper = new Swiper('.swiper-container', {
     },
   },
 });
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true,
+});
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
+  { interval: 100 }
+);
